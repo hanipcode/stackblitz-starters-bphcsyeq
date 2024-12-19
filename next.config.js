7547dab4aae9c8d5872307badc4c709093cf3daf
hanipcode <hanifeij@yahoo.co.id>
2024-12-19 21:38:43 +0700

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Disable SWC minifier
-  swcMinify: false,
-  // Use Babel instead of SWC
-  experimental: {
-    forceSwcTransforms: false
-  }
 };
 
 module.exports = nextConfig;
